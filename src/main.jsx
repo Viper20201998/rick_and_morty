cb220d2390efb9837fa3a0ace422529798f2171a
Viper20201998 <lesterasmethsandoval@gmail.com>
2023-06-22 12:23:51 -0600

    client-id-auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Auth0Provider
 		domain='viper2023.us.auth0.com'
-		clientId='lIKCTZ1ObWaooqqjhtmGWIEDgqOFarL1'
+		clientId='YrDe2nk6KJtoQFiPydVmy5f0qjDNl6uu'
 		authorizationParams={{
 			redirect_uri: window.location.origin,
 		}}
